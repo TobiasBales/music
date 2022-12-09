68ba59a7ce7b2028368878fccc73cfa0b9902285
Tobias Bales <git@tobiasbales.dev>
2022-12-09 23:53:34 +0100

    feat: add accidental and letter name models

---
+++ app/models/accidental.rb
@@ -0,0 +1,12 @@
+# typed: strict
+# frozen_string_literal: true
+
+class Accidental < T::Enum
+  enums do
+    Neutral = new("♮")
+    Sharp = new("♯")
+    Flat = new("♭")
+    DoubleSharp = new("♯♯")
+    DoubleFlat = new("♭♭")
+  end
+end
---
+++ app/models/letter_name.rb
@@ -0,0 +1,14 @@
+# typed: strict
+# frozen_string_literal: true
+
+class LetterName < T::Enum
+  enums do
+    A = new
+    B = new
+    C = new
+    D = new
+    E = new
+    F = new
+    G = new
+  end
+end
