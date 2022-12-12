d846da36a7d3515e7f75c72e04e84477860fc7a0
Tobias Bales <git@tobiasbales.dev>
2022-12-13 00:07:17 +0100

    feat: add seventh chords

--- app/models/chord.rb
+++ app/models/chord.rb
@@ -6,28 +6,58 @@ class Chord < T::Enum
 
   enums do
     Augmented = new
+    AugmentedSeventh = new
     Diminished = new
+    DiminishedMajorSeventh = new
+    DiminishedSeventh = new
+    DominantSeventh = new
+    DominantSeventhFlatFive = new
+    HalfDiminishedSeventh = new
     Major = new
+    MajorSeventh = new
+    MajorSeventhFlatFive = new
     Minor = new
+    MinorMajorSeventh = new
+    MinorSeventh = new
   end
 
-  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength
   sig { returns(T::Array[Interval]) }
   def intervals
     case self
     when Augmented
       [Interval::MajorThird, Interval::AugmentedFifth]
+    when AugmentedSeventh
+      [Interval::MajorThird, Interval::AugmentedFifth, Interval::MinorSeventh]
     when Diminished
       [Interval::MinorThird, Interval::DiminishedFifth]
+    when DiminishedMajorSeventh
+      [Interval::MinorThird, Interval::DiminishedFifth, Interval::MajorSeventh]
+    when DiminishedSeventh
+      [Interval::MinorThird, Interval::DiminishedFifth, Interval::DiminishedSeventh]
+    when DominantSeventh
+      [Interval::MajorThird, Interval::PerfectFifth, Interval::MinorSeventh]
+    when DominantSeventhFlatFive
+      [Interval::MajorThird, Interval::DiminishedFifth, Interval::MinorSeventh]
+    when HalfDiminishedSeventh
+      [Interval::MinorThird, Interval::DiminishedFifth, Interval::MinorSeventh]
     when Major
       [Interval::MajorThird, Interval::PerfectFifth]
+    when MajorSeventh
+      [Interval::MajorThird, Interval::PerfectFifth, Interval::MajorSeventh]
+    when MajorSeventhFlatFive
+      [Interval::MajorThird, Interval::DiminishedFifth, Interval::MajorSeventh]
     when Minor
       [Interval::MinorThird, Interval::PerfectFifth]
+    when MinorMajorSeventh
+      [Interval::MinorThird, Interval::PerfectFifth, Interval::MajorSeventh]
+    when MinorSeventh
+      [Interval::MinorThird, Interval::PerfectFifth, Interval::MinorSeventh]
     else
       T.absurd(self)
     end
   end
-  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/MethodLength
 
   sig { params(root: Note).returns(T::Array[Note]) }
   def for_root(root)
--- test/models/chord_test.rb
+++ test/models/chord_test.rb
@@ -16,11 +16,53 @@ class ChordTest < ActiveSupport::TestCase
     assert_equal [note("c"), note("e♭"), note("g♭")], Chord::Diminished.for_root(Note.deserialize("c"))
   end
 
-  test "c minor" do
-    assert_equal [note("c"), note("e♭"), note("g")], Chord::Minor.for_root(Note.deserialize("c"))
+  test "C+" do
+    assert_equal [note("c"), note("e"), note("g♯")], Chord::Augmented.for_root(Note.deserialize("c"))
   end
 
-  test "c major" do
-    assert_equal [note("c"), note("e"), note("g")], Chord::Major.for_root(Note.deserialize("c"))
+  test "CΔ" do
+    assert_equal [note("c"), note("e"), note("g"), note("b")], Chord::MajorSeventh.for_root(Note.deserialize("c"))
+  end
+
+  test "C−7" do
+    assert_equal [note("c"), note("e♭"), note("g"), note("b♭")], Chord::MinorSeventh.for_root(Note.deserialize("c"))
+  end
+
+  test "C7" do
+    assert_equal [note("c"), note("e"), note("g"), note("b♭")], Chord::DominantSeventh.for_root(Note.deserialize("c"))
+  end
+
+  test "Cø7" do
+    assert_equal [note("c"), note("e♭"), note("g♭"), note("b♭")],
+                 Chord::HalfDiminishedSeventh.for_root(Note.deserialize("c"))
+  end
+
+  test "Co7" do
+    assert_equal [note("c"), note("e♭"), note("g♭"), note("b♭♭")],
+                 Chord::DiminishedSeventh.for_root(Note.deserialize("c"))
+  end
+
+  test "C−Δ7" do
+    assert_equal [note("c"), note("e♭"), note("g"), note("b")],
+                 Chord::MinorMajorSeventh.for_root(Note.deserialize("c"))
+  end
+
+  test "C+7" do
+    assert_equal [note("c"), note("e"), note("g♯"), note("b♭")], Chord::AugmentedSeventh.for_root(Note.deserialize("c"))
+  end
+
+  test "C−Δ7♭5" do
+    assert_equal [note("c"), note("e♭"), note("g♭"), note("b")],
+                 Chord::DiminishedMajorSeventh.for_root(Note.deserialize("c"))
+  end
+
+  test "C7♭5" do
+    assert_equal [note("c"), note("e"), note("g♭"), note("b♭")],
+                 Chord::DominantSeventhFlatFive.for_root(Note.deserialize("c"))
+  end
+
+  test "CΔ7♭5" do
+    assert_equal [note("c"), note("e"), note("g♭"), note("b")],
+                 Chord::MajorSeventhFlatFive.for_root(Note.deserialize("c"))
   end
 end
