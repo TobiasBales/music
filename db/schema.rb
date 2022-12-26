19aca11dfad54abc51d97c5fdb28e4110d99e41f
Tobias Bales <git@tobiasbales.dev>
2022-12-26 23:36:19 +0100

    feat: add basic exercise model

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 20_221_226_223_345) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "exercises", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
