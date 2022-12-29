0581a45e164f545ac86ddf1f6717b2709419e183
Tobias Bales <git@tobiasbales.dev>
2022-12-29 21:21:35 +0100

    feat: allow to to be active record models for crumbs

--- app/components/bootstrap/crumb.rb
+++ app/components/bootstrap/crumb.rb
@@ -5,7 +5,9 @@ module Bootstrap
   class Crumb
     extend T::Sig
 
-    sig { params(text: String, to: String).void }
+    Target = T.type_alias { T.any(String, Symbol, ActiveRecord::Base) }
+
+    sig { params(text: String, to: Target).void }
     def initialize(text:, to:)
       @text = text
       @to = to
@@ -14,7 +16,7 @@ module Bootstrap
     sig { returns(String) }
     attr_reader :text
 
-    sig { returns(String) }
+    sig { returns(Target) }
     attr_reader :to
   end
 end
