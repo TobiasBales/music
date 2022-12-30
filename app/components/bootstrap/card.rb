111661d4dd94080a400a2204e45c2f1e9cc79f0d
Tobias Bales <git@tobiasbales.dev>
2022-12-30 22:20:08 +0100

    feat: add text to card component

--- app/components/bootstrap/card.rb
+++ app/components/bootstrap/card.rb
@@ -5,13 +5,17 @@ module Bootstrap
   class Card < Component
     extend T::Sig
 
-    sig { params(title: T.nilable(String), subtitle: T.nilable(String), image: T.nilable(String)).void }
-    def initialize(title: nil, subtitle: nil, image: nil)
+    sig do
+      params(title: T.nilable(String), subtitle: T.nilable(String), text: T.nilable(String),
+             image: T.nilable(String)).void
+    end
+    def initialize(title: nil, subtitle: nil, text: nil, image: nil)
       super
 
       @title = title
-      @image = image
       @subtitle = subtitle
+      @text = text
+      @image = image
     end
 
     renders_one :body
@@ -24,6 +28,9 @@ module Bootstrap
 
     sig { returns(T.nilable(String)) }
     attr_reader :image
+
+    sig { returns(T.nilable(String)) }
+    attr_reader :text
 
     sig { returns(String) }
     def classes
