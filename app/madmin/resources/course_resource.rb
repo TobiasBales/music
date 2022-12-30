5c1e76e59799ac5efa01d47e9a6e3989014d7231
Tobias Bales <git@tobiasbales.dev>
2022-12-30 20:46:16 +0100

    feat: use nicer name for courses in madmin

--- app/madmin/resources/course_resource.rb
+++ app/madmin/resources/course_resource.rb
@@ -2,6 +2,8 @@
 # frozen_string_literal: true
 
 class CourseResource < Madmin::Resource
+  extend T::Sig
+
   # Attributes
   attribute :id, form: false
   attribute :name
@@ -14,17 +16,18 @@ class CourseResource < Madmin::Resource
   attribute :exercises
   attribute :enrollments
 
-  # Uncomment this to customize the display name of records in the admin area.
-  # def self.display_name(record)
-  #   record.name
-  # end
+  sig { params(record: Course).returns(String) }
+  def self.display_name(record)
+    record.name || super(record)
+  end
 
-  # Uncomment this to customize the default sort column and direction.
-  # def self.default_sort_column
-  #   "created_at"
-  # end
-  #
-  # def self.default_sort_direction
-  #   "desc"
-  # end
+  sig { returns(String) }
+  def self.default_sort_column
+    "created_at"
+  end
+
+  sig { returns(String) }
+  def self.default_sort_direction
+    "desc"
+  end
 end
