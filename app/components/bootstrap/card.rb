bc456c62076be8851996bc45c32c46db78ae9c94
Tobias Bales <git@tobiasbales.dev>
2022-12-30 22:14:12 +0100

    feat: add subtitle to card component

--- app/components/bootstrap/card.rb
+++ app/components/bootstrap/card.rb
@@ -5,18 +5,22 @@ module Bootstrap
   class Card < Component
     extend T::Sig
 
-    sig { params(title: T.nilable(String), image: T.nilable(String)).void }
-    def initialize(title: nil, image: nil)
+    sig { params(title: T.nilable(String), subtitle: T.nilable(String), image: T.nilable(String)).void }
+    def initialize(title: nil, subtitle: nil, image: nil)
       super
 
       @title = title
       @image = image
+      @subtitle = subtitle
     end
 
     renders_one :body
 
     sig { returns(T.nilable(String)) }
     attr_reader :title
+
+    sig { returns(T.nilable(String)) }
+    attr_reader :subtitle
 
     sig { returns(T.nilable(String)) }
     attr_reader :image
