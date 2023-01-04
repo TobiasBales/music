fb245410df5b17d21bf89939c169f116f5478a05
Tobias Bales <git@tobiasbales.dev>
2023-01-04 19:27:06 +0100

    chore: remove hello controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,7 +4,5 @@
 
 import { application } from "./application";
 
-import HelloController from "./hello_controller";
 import TabComponentController from "../../components/tab_component_controller";
-application.register("hello", HelloController);
 application.register("tab-component", TabComponentController);
